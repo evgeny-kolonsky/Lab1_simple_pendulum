58e2f710e05090eed80e452ce0ae40d96d336432
evgeny-kolonsky <126588934+evgeny-kolonsky@users.noreply.github.com>
2024-02-05 16:19:42 +0200

    Rewritten as in "Data Analysis" help material
    + comments

--- pendulum.m
+++ pendulum.m
@@ -30,17 +30,21 @@ hold off
 % Experiment 2: T(l)
 % Theta <= 30 grad
 
+% Place measurements with errobars on the same graph with model
+% T ~ sqrt(l)
+figure(2)
+
 l = [508 480 432 410 332 303 330 370 420 462]; % mm
-T10 = [14.62 14.09 13.39 13.02 11.65 11.22 11.78 12.63 13.19 13.83];
+T10 = [14.62 14.09 13.39 13.02 11.65 11.22 11.78 12.63 13.19 13.83]; % s
 delta_l = L_err ./ l; % relative error
 delta_T = T10_err ./ T10; % relative error
-Tmax = T(1);
+Tmax = T(1); 
 lmax = l(1);
 
-
+% before applying logarythm we have to unitless
 T_unitless = T10 ./ Tmax / 10;
 l_unitless = l / lmax;
-% errors in unitless T and l
+% errors in unitless T and l: the same error for all points
 T_u_err = T10_err /10 /Tmax * ones(10,1);
 l_u_err = L_err /lmax * ones(10,1);
 
@@ -48,7 +52,6 @@ l_u_err = L_err /lmax * ones(10,1);
 l_expected = linspace(0, max(l_unitless));
 T_expected = sqrt(l_expected);
 
-figure(2)
 hold on
 errorbar(l_unitless, T_unitless, T_u_err, T_u_err, l_u_err, l_u_err, '.')
 plot(l_expected, T_expected)
@@ -62,6 +65,12 @@ hold off
 
 
 %% ln axis
+
+
+% Place measurements with errobars on log axis
+% and build linear regression
+figure(3)
+
 Tmax = T10(1);
 lmax = l(1);
 lnT = log(T10/Tmax);
@@ -70,7 +79,6 @@ lnl = log(l/lmax);
 lnT_err = delta_T ./ (T10 / Tmax);
 lnl_err = delta_l ./ (l / lmax);
 
-figure(3)
 hold on
 % measurements with error bars
 errorbar(lnl, lnT, lnT_err, lnT_err, lnl_err, lnl_err, 'o')
